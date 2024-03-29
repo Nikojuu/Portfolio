fc514426479a638c3da266a988615c968c93f0d1
Nikojuu <come_on12345@hotmail.com>
2024-03-29 22:37:21 +0200

    updates

--- src/js/observers.js
+++ src/js/observers.js
@@ -3,14 +3,18 @@ const cards = document.querySelectorAll(".card__skills");
 const skills = document.querySelectorAll(".card__skills__skill");
 const projectsImg = document.querySelectorAll(".projects__image");
 const verticalLineLeft = document.querySelectorAll(".vertical__line-left");
+const verticalLineLeft2 = document.querySelector(".vertical__line-left-2");
 const verticalLineRight = document.querySelector(".vertical__line-right");
 const horizontalLineLeft = document.querySelectorAll(".horizontal__line-left");
+const horizontalLineLeft2 = document.querySelector(".horizontal__line-left-2");
 const horizontalLineRight = document.querySelector(".horizontal__line-right");
 const projectsDescription = document.querySelectorAll(".projects__description");
 const leftArticle = document.querySelectorAll(".article__left");
 const articleText = document.querySelectorAll(".article p");
 const sertificates = document.querySelector(".sertificate");
+const projects = document.querySelector(".projects");
 const sertificateCards = document.querySelectorAll(".sertificate__card");
+const projectCards = document.querySelectorAll(".projects__card");
 const serviceContainer = document.querySelector(".services-container");
 const serviceCards = document.querySelectorAll(".services-card");
 // helper waitTime function just pass Millseconds you want to wait!
@@ -75,7 +79,7 @@ projectsImg.forEach((img) => {
         }
         if (
           entry.isIntersecting &&
-          entry.target.classList.contains("projects__image-2")
+          entry.target.classList.contains("projects__image-1")
         ) {
           projectsDescription[1].style.animation =
             "slideIn-left 1.5s cubic-bezier(0.6, -0.05, 0.1, 0.99) both";
@@ -83,9 +87,22 @@ projectsImg.forEach((img) => {
 
           horizontalLineRight.classList.add("horizontal__line");
           imgObserver.unobserve(entry.target);
-        } else if (
+        }
+        if (
           entry.isIntersecting &&
-          entry.target.classList.contains("projects__image-1")
+          entry.target.classList.contains("projects__image-2")
+        ) {
+          projectsDescription[2].style.animation =
+            "slideIn-right 1.5s cubic-bezier(0.6, -0.05, 0.1, 0.99) both";
+          verticalLineLeft2.classList.add("vertical__line");
+
+          horizontalLineLeft2.classList.add("horizontal__line");
+          imgObserver.unobserve(entry.target);
+        }
+
+        if (
+          entry.isIntersecting &&
+          entry.target.classList.contains("projects__image-3")
         ) {
           // vertical and horizontal line [0] as in first instance projects section in this case
           verticalLineLeft[0].classList.add("vertical__line");
@@ -137,6 +154,12 @@ const observerSertificates = new IntersectionObserver(
       observerSertificates.unobserve(sertificates);
     } else if (
       entry.isIntersecting &&
+      entry.target.classList.contains("projects")
+    ) {
+      cardAnimation(projectCards);
+      observerSertificates.unobserve(projects);
+    } else if (
+      entry.isIntersecting &&
       entry.target.classList.contains("services-container")
     ) {
       cardAnimation(serviceCards);
@@ -150,6 +173,7 @@ const observerSertificates = new IntersectionObserver(
 
 observerSertificates.observe(leftArticle[0]);
 observerSertificates.observe(sertificates);
+observerSertificates.observe(projects);
 observerSertificates.observe(leftArticle[1]);
 observerSertificates.observe(serviceContainer);
 // Sertificate functions//
